a212e2bad987e2c2e8f351765f4fcec24bef0a74
Vincent Tirgei <tirgeic@gmail.com>
2019-01-15 21:05:00 +0300

    [Finishes #163221842] Add create_app function to initialize Flask app

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from instance.config import app_config
+
+
+def create_app(config_name):
+    """ Function to initialize Flask app """
+
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object(app_config[config_name])
+    app.config.from_pyfile('config.py')
+
+    return app
