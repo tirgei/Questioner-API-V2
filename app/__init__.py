61f30083fa10c1e0f874fff16ae85a77b63d0f93
Vincent Tirgei <tirgeic@gmail.com>
2019-01-16 23:59:57 +0300

    [Finishes #163222574] Add JWT Manager

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from instance.config import app_config
 from db.db_config import connect_db
 from db.db_tables import create_tables
 from app.api import v2
+from flask_jwt_extended import (JWTManager)
 
 
 def create_app(config_name):
@@ -11,6 +12,8 @@ def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
+
+    jwt = JWTManager(app)
 
     conn = connect_db()
     create_tables(conn)
