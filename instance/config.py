148b43f3e8f8777e02b891630b8b5bf850cd8e6b
Vincent Tirgei <tirgeic@gmail.com>
2019-01-16 11:40:47 +0300

    [Chore #163222503] Update encvironment config

--- instance/config.py
+++ instance/config.py
@@ -1,26 +1,36 @@
+import os
+
+
 class Config(object):
     """ Base Config class """
 
     DEBUG = False
     TESTING = False
+    DATABASE_HOST = os.getenv('DATABASE_HOST')
+    DATABASE_PORT = os.getenv('DATABASE_PORT')
+    DATABASE_USERNAME = os.getenv('DATABASE_USERNAME')
+    DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
 
 
 class DevelopmentConfig(Config):
     """ Config class for Development environment """
 
     DEBUG = True
+    DATABASE_NAME = os.getenv('DATABASE_NAME')
 
 
 class TestingConfig(Config):
     """ Config class for Testing environment """
 
     TESTING = True
+    DATABASE_NAME = os.getenv('TEST_DATABASE_NAME')
 
 
 class StagingConfig(Config):
     """ Config class for Staging environment """
 
     DEBUG = True
+    DATABASE_NAME = os.getenv('TEST_DATABASE_NAME')
 
 
 class ProductionConfig(Config):
