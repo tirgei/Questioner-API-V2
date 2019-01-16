6af5fcbe6ae98c88e74c2678daded0de9de9a1e1
Vincent Tirgei <tirgeic@gmail.com>
2019-01-16 18:45:34 +0300

    [Feature #163222574] Add function to initialize tables

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from instance.config import app_config
+from db.db_config import connect_db
+from db.db_tables import create_tables
 from app.api import v2
 
 
@@ -9,6 +11,9 @@ def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
+
+    conn = connect_db()
+    create_tables(conn)
 
     app.register_blueprint(v2)
 
