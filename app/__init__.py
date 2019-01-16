d66a86d90ff9d253c7f96ed2e2680aaaec8b71d3
Vincent Tirgei <tirgeic@gmail.com>
2019-01-16 16:12:38 +0300

    [Feature #163222574] Register user signup Blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from instance.config import app_config
+from app.api import v2
 
 
 def create_app(config_name):
@@ -8,5 +9,7 @@ def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
+
+    app.register_blueprint(v2)
 
     return app
