0ca21db8e2d3356fe37c77b967d5673cb9132cf8
Vincent Tirgei <tirgeic@gmail.com>
2019-01-16 18:47:26 +0300

    Update base test

--- app/tests/base_test.py
+++ app/tests/base_test.py
@@ -1,8 +1,10 @@
+import os
 import unittest
 from flask import g
 from app import create_app
-from manage import migrate, truncate
+from db.db_tables import drop_tables
 from app.api.models.user_model import UserModel
+from app.api.utils.database_model import DatabaseModel
 
 
 class BaseTest(unittest.TestCase):
@@ -11,14 +13,15 @@ class BaseTest(unittest.TestCase):
     def setUp(self):
         """ Setup database """
 
+        os.environ['APP_ENV'] = 'testing'
+
         self.app = create_app('testing')
         self.app_context = self.app.app_context()
         self.app_context.push()
 
-        self.db = UserModel()
+        self.db = DatabaseModel()
         self.client = self.app.test_client()
-        migrate(g.conn)
 
     def tearDown(self):
         """ Clear database """
-        truncate(g.conn)
+        drop_tables(g.conn)
