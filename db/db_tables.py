d9899e84df9b607a88f948870aa4eaddbd964a3b
Vincent Tirgei <tirgeic@gmail.com>
2019-01-16 11:41:54 +0300

    [Chore #163222503] Add function to manage db

---
+++ db/db_tables.py
@@ -0,0 +1,114 @@
+from werkzeug.security import generate_password_hash
+
+tables = [
+    'users',
+    'meetups',
+    'questions',
+    'comments'
+]
+
+create_table_queries = [
+    """
+    CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY NOT NULL,
+        firstname VARCHAR(250) NOT NULL,
+        lastname VARCHAR(250) NOT NULL,
+        othername VARCHAR(250) NULL,
+        username VARCHAR(250) NOT NULL UNIQUE,
+        phonenumber VARCHAR(250) NULL,
+        email VARCHAR(250) NOT NULL UNIQUE,
+        password VARCHAR(250) NOT NULL,
+        registered TIMESTAMP WITHOUT TIME ZONE \
+        DEFAULT (NOW() AT TIME ZONE 'utc'),
+        admin BOOLEAN NOT NULL DEFAULT FALSE
+    )
+    """,
+
+    """
+    CREATE TABLE IF NOT EXISTS meetups (
+        id SERIAL PRIMARY KEY NOT NULL,
+        topic VARCHAR(250) NOT NULL,
+        description VARCHAR(250) NOT NULL,
+        location VARCHAR(250) NOT NULL,
+        happening_on TIMESTAMP WITHOUT TIME ZONE \
+        DEFAULT (NOW() AT TIME ZONE 'utc'),
+        created_at TIMESTAMP WITHOUT TIME ZONE \
+        DEFAULT (NOW() AT TIME ZONE 'utc'),
+        modified_at TIMESTAMP WITHOUT TIME ZONE \
+        DEFAULT (NOW() AT TIME ZONE 'utc')
+    )
+    """,
+
+    """
+    CREATE TABLE IF NOT EXISTS questions (
+        id SERIAL PRIMARY KEY NOT NULL,
+        topic VARCHAR(250) NULL,
+        body VARCHAR(250) NOT NULL,
+        votes INTEGER NOT NULL DEFAULT 0,
+        meetup_id INTEGER NOT NULL,
+        user_id INTEGER NOT NULL,
+        created_at TIMESTAMP WITHOUT TIME ZONE \
+        DEFAULT (NOW() AT TIME ZONE 'utc'),
+        modified_at TIMESTAMP WITHOUT TIME ZONE \
+        DEFAULT (NOW() AT TIME ZONE 'utc'),
+        FOREIGN KEY (meetup_id) REFERENCES meetups(id),
+        FOREIGN KEY (user_id) REFERENCES users(id)
+    )
+    """,
+
+    """
+    CREATE TABLE IF NOT EXISTS comments (
+        id SERIAL PRIMARY KEY NOT NULL,
+        body VARCHAR(250) NULL,
+        question_id INTEGER NOT NULL,
+        user_id INTEGER NOT NULL,
+        created_at TIMESTAMP WITHOUT TIME ZONE \
+        DEFAULT (NOW() AT TIME ZONE 'utc'),
+        modified_at TIMESTAMP WITHOUT TIME ZONE \
+        DEFAULT (NOW() AT TIME ZONE 'utc'),
+        FOREIGN KEY (question_id) REFERENCES questions(id),
+        FOREIGN KEY (user_id) REFERENCES users(id)
+    )
+    """
+]
+
+
+def truncate(connection):
+    """ Function to truncate database tables """
+
+    cur = connection.cursor()
+    cur.execute('TRUNCATE TABLE ' + ','.join(tables) + ' CASCADE')
+    connection.commit()
+
+
+def drop_tables(connection):
+    """ Function to drop tables """
+
+    cur = connection.cursor()
+    for table in tables:
+        cur.execute('DROP TABLE IF EXISTS {} CASCADE'.format(table))
+
+    connection.commit()
+
+
+def create_tables(connection):
+    """ Function to create tables """
+
+    cur = connection.cursor()
+    for query in create_table_queries:
+        cur.execute(query)
+
+    connection.commit()
+
+
+def seed_db(connection):
+    """ Function to add super user to db """
+
+    cur = connection.cursor()
+    password = generate_password_hash('asf#242KL8')
+
+    cur.execute("INSERT INTO users (firstname, lastname, username, \
+    email, password, admin) VALUES ('Vincent', 'Tirgei', 'tirgei', \
+    'tirgeic@gmail.com', {}, True)".format(password))
+
+    connection.commit()
