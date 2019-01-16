0223882e72befa4293bce985833c18f4ff74af30
Vincent Tirgei <tirgeic@gmail.com>
2019-01-16 18:46:53 +0300

    [Feature #163222574] Update Database Model

--- app/api/utils/base_model.py
+++ app/api/utils/base_model.py
@@ -1,11 +1,12 @@
 from abc import ABC, abstractmethod
+from ..utils.database_model import DatabaseModel
 
 
-class Model(ABC):
+class Model(ABC, DatabaseModel):
     """ Base model class for objects """
 
     def __init__(self):
-        pass
+        super().__init__()
 
     @abstractmethod
     def save(self, data):
--- app/api/utils/database_model.py
+++ app/api/utils/database_model.py
@@ -1,17 +1,16 @@
 from flask import g
 from db.db_config import connect_db
-from db.db_tables import create_tables, drop_tables
-from .base_model import Model
 
 
-class DatabaseModel(Model):
+class DatabaseModel():
     """ Class to initiate database connection """
 
     def __init__(self):
         self.conn = self.get_db_connection()
         self.cur = self.conn.cursor()
 
-    def get_db_connection(self):
+    @staticmethod
+    def get_db_connection():
         """ Function to check if database connection exists """
 
         if 'conn' not in g:
