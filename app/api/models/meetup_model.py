798f559c7644b614ee004f48e7391c145452fef8
Vincent Tirgei <tirgeic@gmail.com>
2019-01-17 11:51:04 +0300

    [Feature #163222687] Create meetup model

---
+++ app/api/models/meetup_model.py
@@ -0,0 +1,28 @@
+from ..utils.base_model import Model
+
+
+class MeetupModel(Model):
+    """ Model class for meetup object """
+
+    table = 'meetups'
+
+    def save(self, data):
+        """ Function to save new user """
+
+        query = "INSERT INTO {} (topic, description, location, happening_on)\
+        VALUES ('{}', '{}', '{}', '{}') RETURNING *".format(
+            self.table, data['topic'],
+            data['description'], data['location'], data['happening_on']
+        )
+
+        self.cur.execute(query)
+        result = self.cur.fetchone()
+
+        self.conn.commit()
+        return result
+
+    def exists(self, key, value):
+        pass
+    
+    def where(self, key, where):
+        pass
