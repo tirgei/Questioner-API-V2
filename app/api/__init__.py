4a6124d7766593e8b92063c94adc1be56d7b6276
Vincent Tirgei <tirgeic@gmail.com>
2019-01-17 15:32:37 +0300

    [Feature #163223989] Add resource for specific meetup to Blueprint

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,7 +1,7 @@
 from flask_restful import Api
 from flask import Blueprint
 from .views.user_view import Register, Login, RefreshToken
-from .views.meetup_view import Meetups
+from .views.meetup_view import Meetups, Meetup
 
 v2 = Blueprint('version_2', __name__, url_prefix='/api/v2')
 
@@ -11,3 +11,4 @@ api.add_resource(Register, '/auth/signup')
 api.add_resource(Login, '/auth/login')
 api.add_resource(RefreshToken, '/refresh-token')
 api.add_resource(Meetups, '/meetups')
+api.add_resource(Meetup, '/meetups/<int:meetup_id>')
