450d11806ed6b37c1d26e16cdd1f77725dfa4e9a
Vincent Tirgei <tirgeic@gmail.com>
2019-01-17 07:42:45 +0300

    [Feature #163222620] Add login resource to Api

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,9 +1,10 @@
 from flask_restful import Api
 from flask import Blueprint
-from .views.user_view import Register
+from .views.user_view import Register, Login
 
 v2 = Blueprint('version_2', __name__, url_prefix='/api/v2')
 
 api = Api(v2)
 
 api.add_resource(Register, '/auth/signup')
+api.add_resource(Login, '/auth/login')
