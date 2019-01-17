306e4cb87bca5fdf426b770ce9406c2662dac9a9
Vincent Tirgei <tirgeic@gmail.com>
2019-01-17 07:40:52 +0300

    [Start #163222620] Add tests for user login

--- app/tests/test_users.py
+++ app/tests/test_users.py
@@ -134,3 +134,86 @@ class TestUser(BaseTest):
         self.assertEqual(res.status_code, 409)
         self.assertEqual(data['status'], 409)
         self.assertEqual(data['message'], 'Email already exists')
+
+    def test_login_no_data(self):
+        """ Test login with no data provided """
+
+        res = self.client.post('/api/v2/auth/login')
+        data = res.get_json()
+
+        self.assertEqual(res.status_code, 400)
+        self.assertEqual(data['status'], 400)
+        self.assertEqual(data['message'], 'No data provided')
+
+    def test_login_empty_data(self):
+        """ Test login with empty data provided """
+
+        self.user.clear()
+
+        res = self.client.post('/api/v2/auth/login', json=self.user)
+        data = res.get_json()
+
+        self.assertEqual(res.status_code, 400)
+        self.assertEqual(data['status'], 400)
+        self.assertEqual(data['message'], 'No data provided')
+
+    def test_login_unregistered_user(self):
+        """ Test login with unregistered user credentials """
+
+        res = self.client.post('/api/v2/auth/login', json=self.user)
+        data = res.get_json()
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['status'], 404)
+        self.assertEqual(data['message'], 'User not found')
+
+    def test_login_successfully(self):
+        """ Test successfull login """
+
+        self.client.post('/api/v2/auth/signup', json=self.user)
+
+        res = self.client.post('/api/v2/auth/login', json=self.user)
+        data = res.get_json()
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['status'], 200)
+        self.assertEqual(data['message'], 'User logged in successfully')
+
+    def test_login_no_username_provided(self):
+        """ Test login with no username provided """
+
+        self.client.post('/api/v2/auth/signup', json=self.user)
+        self.user.pop('username', None)
+
+        res = self.client.post('/api/v2/auth/login', json=self.user)
+        data = res.get_json()
+
+        self.assertEqual(res.status_code, 400)
+        self.assertEqual(data['status'], 400)
+        self.assertEqual(data['message'], 'Invalid credentials')
+
+    def test_login_invalid_password(self):
+        """ Test login with invalid password """
+
+        self.client.post('/api/v2/auth/signup', json=self.user)
+        self.user.update({'password': 'asfdgfdngf'})
+
+        res = self.client.post('/api/v2/auth/login', json=self.user)
+        data = res.get_json()
+
+        self.assertEqual(res.status_code, 400)
+        self.assertEqual(data['status'], 400)
+        self.assertEqual(data['message'], 'Invalid data provided')
+
+    def test_login_incorrect_password(self):
+        """ Test login with invalid password """
+
+        self.client.post('/api/v2/auth/signup', json=self.user)
+        self.user.update({'password': 'zdf7#sdfN'})
+
+        res = self.client.post('/api/v2/auth/login', json=self.user)
+        data = res.get_json()
+
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data['status'], 422)
+        self.assertEqual(data['message'], 'Incorrect password')
