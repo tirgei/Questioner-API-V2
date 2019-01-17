d117f8f0e7b63101ab2f9cae56972a514b9c7aa2
Vincent Tirgei <tirgeic@gmail.com>
2019-01-18 02:38:57 +0300

    [Feature #163225131] Add votes model

---
+++ app/api/models/vote_model.py
@@ -0,0 +1,41 @@
+from ..utils.database_model import DatabaseModel
+
+
+class VoteModel(DatabaseModel):
+    """ Model class for vote """
+
+    table = 'votes'
+
+    def voted(self, question_id, user_id):
+        """ Function to check user has voted """
+
+        query = "SELECT * FROM {} WHERE question_id = '{}' AND user_id = '{}'\
+        ".format(self.table, question_id, user_id)
+
+        self.cur.execute(query)
+        result = self.cur.fetchone()
+        return result
+
+    def add(self, data):
+        """ Function to add new vote """
+
+        query = "INSERT INTO {} (question_id, user_id, vote) VALUES\
+        ('{}', '{}', '{}')".format(
+            self.table, data['question_id'], data['user_id'], data['vote']
+        )
+
+        self.cur.execute(query)
+        self.conn.commit()
+
+    def update(self, data):
+        """ Function to add new vote """
+
+        q_id = data['question_id']
+        u_id = data['user_id']
+        vote = data['vote']
+
+        query = "UPDATE {} SET vote = '{}' WHERE question_id = '{}' AND user_id = '{}'\
+        ".format(self.table, vote, q_id, u_id)
+
+        self.cur.execute(query)
+        self.conn.commit()
