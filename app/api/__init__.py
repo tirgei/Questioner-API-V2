7d8708a103167f109717f13bfda6b657d075de5e
Vincent Tirgei <tirgeic@gmail.com>
2019-01-17 18:54:34 +0300

    [Finishes #163225285] Add all questions resource to Blueprint

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -2,7 +2,7 @@ from flask_restful import Api
 from flask import Blueprint
 from .views.user_view import Register, Login, RefreshToken
 from .views.meetup_view import Meetups, Meetup
-from .views.question_view import Question
+from .views.question_view import Question, QuestionList
 
 v2 = Blueprint('version_2', __name__, url_prefix='/api/v2')
 
@@ -14,3 +14,4 @@ api.add_resource(RefreshToken, '/refresh-token')
 api.add_resource(Meetups, '/meetups')
 api.add_resource(Meetup, '/meetups/<int:meetup_id>')
 api.add_resource(Question, '/questions')
+api.add_resource(QuestionList, '/meetups/<int:meetup_id>/questions')
