68b9af92dfa0cbce9cde07b035a30e6d5c2b6d1d
Vincent Tirgei <tirgeic@gmail.com>
2019-01-17 11:52:16 +0300

    [Feature #163222687] Add meetups Resource

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,6 +1,7 @@
 from flask_restful import Api
 from flask import Blueprint
 from .views.user_view import Register, Login, RefreshToken
+from .views.meetup_view import Meetups
 
 v2 = Blueprint('version_2', __name__, url_prefix='/api/v2')
 
@@ -9,3 +10,4 @@ api = Api(v2)
 api.add_resource(Register, '/auth/signup')
 api.add_resource(Login, '/auth/login')
 api.add_resource(RefreshToken, '/refresh-token')
+api.add_resource(Meetups, '/meetups')
