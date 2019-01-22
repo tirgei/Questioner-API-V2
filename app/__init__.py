4651f6708bd993b2b9aa2911b73b66d8d818a2c0
Vincent Tirgei <tirgeic@gmail.com>
2019-01-22 13:40:30 +0300

    [Finishes #163384755] Setup flask-cors

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from db.db_tables import create_tables, seed
 from app.api import v2
 from flask_jwt_extended import (JWTManager)
 from app.api.models.token_model import RevokedTokenModel
+from flask_cors import CORS
 
 
 @v2.route('/', methods=['GET'])
@@ -23,6 +24,7 @@ def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
+    CORS(app)
 
     jwt = JWTManager(app)
 
